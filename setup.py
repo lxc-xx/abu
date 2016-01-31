c348b0c2a4afccd7a6acefb7567bd20eb07187c6
Xuanchong Li <lxc@CMU-871920.WV.CC.CMU.EDU>
2016-01-31 14:30:49 -0500

    upload to pip

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup( name = 'abu', 
+        packages = ['abu'], 
+        version = '0.1', 
+        description = 'An AWS Cluster Manager', 
+        author = 'Xuanchong Li', 
+        author_email = 'lixuanchong@yahoo.com', 
+        url = 'https://github.com/lxc-xx/abu',
+        download_url = 'https://github.com/lxc-xx/abu/tarball/0.1', 
+        keywords = ['aws', 'cluster', 'scheduler'], 
+        classifiers = [],)
