743c91b5299aa7f692ef73ea88db75fa7e2b70b7
Xiaojun Chang <cxj273@gmail.com>
2016-02-21 23:23:05 +0000

    0.9

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup( name = 'abu', 
         packages = ['abu'], 
-        version = '0.8', 
+        version = '0.9', 
         description = 'An AWS Cluster Manager', 
         author = 'Xuanchong Li', 
         author_email = 'lixuanchong@yahoo.com', 
         url = 'https://github.com/lxc-xx/abu',
-        download_url = 'https://github.com/lxc-xx/abu/tarball/0.8',
+        download_url = 'https://github.com/lxc-xx/abu/tarball/0.9',
         keywords = ['aws', 'cluster', 'scheduler'], 
         classifiers = [],)
